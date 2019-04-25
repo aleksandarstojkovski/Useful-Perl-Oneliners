95ed0328a2a10c22bbfe8460b57a9387c7aab890
Aleksandar Stojkovski <43863010+aleksandarstojkovski@users.noreply.github.com>
2019-04-25 17:33:19 +0200

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -1,11 +1,13 @@
 ##########################################################
 # last occurrence of regular expression
 ##########################################################
+
 Command   :   perl -n -e '$e = $1 if /(REGEX_HERE)/;  END{ print $e }'
 
 ##########################################################
 # print something if regex matches
 ##########################################################
+
 Command   :   perl -ne 'print /(REGEX_HERE)/ ? "match" : "no-match" '
 Example   :   echo "ABC123" | perl -ne 'print /ABC123/ ? "match" : "no-match" '  
 Output    :   match
@@ -13,9 +15,7 @@ Output    :   match
 ##########################################################
 # print content of specific column (separated by spaces)
 ##########################################################
-# $F[] is the array of the columns,
-# $F[0] is the first column, $F[n] is the "n" column
-##########################################################
+
 Command   :   perl -lane 'print $F[0]'
 Example   :   echo "A B C" | perl -lane 'print $F[0]'
 Output    :   A
@@ -23,9 +23,15 @@ Output    :   A
 ##########################################################
 # print content of specific column (separated by any character)
 ##########################################################
-# $F[] is the array of the columns,
-# $F[0] is the first column, $F[n] is the "n" column
-##########################################################
+
 Command   :   perl -F[CHARACTER_HERE] -a -ne 'print "$F[0]" '
 Example   :   echo "A;B;C" | perl -F; -a -ne 'print "$F[1]" '
 Output    :   B
+
+##########################################################
+# print something if columns match specific regex
+##########################################################
+
+Command   :   perl -lane 'print "TRUE" if $F[0]=~/\d/ and $F[1]=~/\D/'
+Example   :   echo "3 z" | perl -lane 'print "TRUE" if $F[0]=~/\d/ and $F[1]=~/\D/'
+Output    :   TRUE
