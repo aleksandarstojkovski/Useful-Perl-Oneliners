baf131ae30b1511bce2376d12aaec67b5d6000e9
Aleksandar Stojkovski <43863010+aleksandarstojkovski@users.noreply.github.com>
2019-04-25 17:28:34 +0200

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -1,2 +1,31 @@
-// last occurrence of regular expression
-perl -n -e '$e = $1 if /(REGEX_HERE)/;  END{ print $e }'
+##########################################################
+# last occurrence of regular expression
+##########################################################
+Command   :   perl -n -e '$e = $1 if /(REGEX_HERE)/;  END{ print $e }'
+
+##########################################################
+# print something if regex matches
+##########################################################
+Command   :   perl -ne 'print /(REGEX_HERE)/ ? "match" : "no-match" '
+Example   :   echo "ABC123" | perl -ne 'print /ABC123/ ? "match" : "no-match" '  
+Output    :   match
+
+##########################################################
+# print content of specific column (separated by spaces)
+##########################################################
+# $F[] is the array of the columns,
+# $F[0] is the first column, $F[n] is the "n" column
+##########################################################
+Command   :   perl -lane 'print $F[0]'
+Example   :   echo "A B C" | perl -lane 'print $F[0]'
+Output    :   A
+
+##########################################################
+# print content of specific column (separated by any character)
+##########################################################
+# $F[] is the array of the columns,
+# $F[0] is the first column, $F[n] is the "n" column
+##########################################################
+Command   :   perl -F[CHARACTER_HERE] -a -ne 'print "$F[0]" '
+Example   :   echo "A;B;C" | perl -F; -a -ne 'print "$F[1]" '
+Output    :   B
